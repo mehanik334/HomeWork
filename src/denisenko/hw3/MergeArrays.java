7e1d7c4b0ac7cb2d29f4d2ff836a498915269a86
mehanik334 <meha334@gmail.com>
2019-03-28 23:13:09 +0200

    reincarnation MergeArrays

--- src/denisenko/hw3/MergeArrays.java
+++ src/denisenko/hw3/MergeArrays.java
@@ -4,63 +4,42 @@ public class MergeArrays {
 
     public static int[] mergeTwoArrays(int[] arrayOne, int[] arrayTwo) {
 
-        int[] newArray = new int[arrayOne.length + arrayTwo.length];
-        int k = 0;
-        for (int i = 0; i < arrayOne.length; i++) {
+        int[] resArray = new int[arrayOne.length + arrayTwo.length];
+        int indArrOne = 0;
+        int indArrTwo = 0;
 
-            for (int j = 0; j < arrayTwo.length; j++) {
+        for (int i = 0; i < resArray.length; i++) {
 
-                if ((arrayOne.length == 1) && (arrayOne[0] < arrayTwo[0]) && (arrayTwo.length == 1)) {
+            if (indArrOne > arrayOne.length - 1) {
 
-                    newArray[newArray.length - 2] = arrayOne[0];
-                    newArray[newArray.length - 1] = arrayTwo[0];
+                resArray[i] = arrayTwo[indArrTwo];
+                indArrTwo++;
 
-                }else if (arrayOne[i] < arrayTwo[j]) {
+            } else if ((indArrTwo > arrayTwo.length - 1) || (arrayOne[indArrOne] < arrayTwo[indArrTwo])) {
 
-                    newArray[i + j + k] = arrayOne[i];
-                    arrayOne = removeFistItem(arrayOne);
-                    k++;
-                    i--;
-                    break;
+                resArray[i] = arrayOne[indArrOne];
+                indArrOne++;
 
-                } else {
+            } else {
 
-                    newArray[i + j + k] = arrayTwo[j];
-                    arrayTwo = removeFistItem(arrayTwo);
-
-                    if (arrayTwo.length == 0) {
-
-                        for (int g = k + 1, h = 0; g < newArray.length; g++, h++) {
-                            newArray[g] = arrayOne[h];
-                        }
-                    }
-                    j--;
-                    k++;
-
-                }
+                resArray[i] = arrayTwo[indArrTwo];
+                indArrTwo++;
 
             }
-
         }
-        return newArray;
+        return resArray;
     }
 
-    private static int[] removeFistItem(int[] ar) {
-
-        int[] result = new int[ar.length - 1];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = ar[i + 1];
-        }
-        return result;
-    }
 
     public static void main(String[] args) {
 
-        int[] ar1 = {1, 5, 7, 12};
-        int[] ar2 = {0, 1, 7};
+        int[] ar1 = {0};
+        int[] ar2 = {3, 4};
         int[] res = mergeTwoArrays(ar1, ar2);
         for (int i = 0; i < res.length; i++) {
+
             System.out.println(res[i]);
+
         }
     }
 }
